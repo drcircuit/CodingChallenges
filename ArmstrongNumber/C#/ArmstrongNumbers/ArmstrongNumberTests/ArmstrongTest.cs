a0e4ed64197c87d7649d767ecaa2ca7bdc3f6f63
drcircuit <espen@sandelarsen.com>
2016-03-04 09:08:59 +0100

    created solution for Armstrong Number Check

---
+++ ArmstrongNumber/C#/ArmstrongNumbers/ArmstrongNumberTests/ArmstrongTest.cs
@@ -0,0 +1,38 @@
+﻿using ArmstrongNumber;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ArmstrongNumberTests
+{
+    [TestClass]
+    public class ArmstrongTest {
+        [TestMethod]
+        public void ZeroIsArmstrongNumber()
+        {
+            Assert.IsTrue(0.IsArmstrong());
+        }
+
+        [TestMethod]
+        public void OneIsArmstrongNumber()
+        {
+            Assert.IsTrue(1.IsArmstrong());
+        }
+
+        [TestMethod]
+        public void NoNumberAboveOneAndBelowTenIsArmstrong()
+        {
+            Assert.IsFalse(2.IsArmstrong());
+        }
+
+        [TestMethod]
+        public void NonArmstrongNumberIsArmstrong()
+        {
+            Assert.IsFalse(999.IsArmstrong());
+        }
+
+        [TestMethod]
+        public void ArmstrongNumberIsArmstrong()
+        {
+            Assert.IsTrue(407.IsArmstrong());
+        }
+    }
+}
---
+++ ArmstrongNumber/C#/ArmstrongNumbers/ArmstrongNumbers/ArmstrongIntExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace ArmstrongNumber
+{
+    public static class ArmstrongIntExtensions
+    {
+        public static bool IsArmstrong(this int number)
+        {
+            int n = number, sum = 0;
+
+            while(number > sum)
+            {
+                var r = n % 10;
+                sum += r * r * r;
+                n /= 10;
+            }
+            return sum == number;
+        }
+    }
+}
