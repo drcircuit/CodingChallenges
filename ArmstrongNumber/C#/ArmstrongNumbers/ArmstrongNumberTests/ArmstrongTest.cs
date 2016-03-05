01240f2f34af71b5ddf653bf1396a5cd7897aa32
drcircuit <espen@sandelarsen.com>
2016-03-05 18:10:27 +0100

    Armstrong / Narcisistic Number DONE!

--- ArmstrongNumber/C#/ArmstrongNumbers/ArmstrongNumberTests/ArmstrongTest.cs
+++ ArmstrongNumber/C#/ArmstrongNumbers/ArmstrongNumberTests/ArmstrongTest.cs
@@ -6,33 +6,113 @@ namespace ArmstrongNumberTests
     [TestClass]
     public class ArmstrongTest {
         [TestMethod]
-        public void ZeroIsArmstrongNumber()
+        public void AllSingleDigitNumbersAreArmstrong()
         {
-            Assert.IsTrue(0.IsArmstrong());
+            int n = 0;
+            while (n < 10)
+                Assert.AreEqual(true,n++.IsArmstrong());
         }
 
         [TestMethod]
-        public void OneIsArmstrongNumber()
+        public void NoDoubleDigitNumbersAreArmstrong()
         {
-            Assert.IsTrue(1.IsArmstrong());
+            int n = 10;
+            while (n < 100)
+                Assert.AreEqual(false,n++.IsArmstrong());
         }
 
         [TestMethod]
-        public void NoNumberAboveOneAndBelowTenIsArmstrong()
+        public void ThreeDigitNonArmstrongAreNotArmstrong()
         {
-            Assert.IsFalse(2.IsArmstrong());
+            Assert.AreEqual(false, 100.IsArmstrong());
         }
 
         [TestMethod]
-        public void NonArmstrongNumberIsArmstrong()
+        public void ThreeDigitArmstrongsAreArmstrong()
         {
-            Assert.IsFalse(999.IsArmstrong());
+            Assert.AreEqual(true, 153.IsArmstrong());
+            Assert.AreEqual(true, 370.IsArmstrong());
+            Assert.AreEqual(true, 371.IsArmstrong());
+            Assert.AreEqual(true, 407.IsArmstrong());
         }
 
         [TestMethod]
-        public void ArmstrongNumberIsArmstrong()
+        public void FourDigitArmstrongAreArmstrong()
         {
-            Assert.IsTrue(407.IsArmstrong());
+            Assert.AreEqual(true,1634.IsArmstrong());
+            Assert.AreEqual(true, 8208.IsArmstrong());
+            Assert.AreEqual(true, 9474.IsArmstrong());
+        }
+
+        [TestMethod]
+        public void BigArmstrongAreArmstrong()
+        {
+            Assert.AreEqual(true, 24678051.IsArmstrong());
+            Assert.AreEqual(true, 146511208.IsArmstrong());
+        }
+    }
+
+    [TestClass]
+    public class RaiseToPowerTest
+    {
+        [TestMethod]
+        public void ZeroToTheZeroethPowerIsOne()
+        {
+            Assert.AreEqual(1, 0.Raise(0));
+        }
+
+        [TestMethod]
+        public void PositiveToZeroethPowerIsZero()
+        {
+            Assert.AreEqual(0, 1.Raise(0));
+        }
+
+        [TestMethod]
+        public void SquaredIsSquared()
+        {
+            Assert.AreEqual(4, 2.Raise(2));
+            Assert.AreEqual(16, 4.Raise(2));
+            Assert.AreEqual(100, 10.Raise(2));
+            Assert.AreEqual(65536, 256.Raise(2));
+        }
+
+        [TestMethod]
+        public void CubedIsCubed()
+        {
+            Assert.AreEqual(8, 2.Raise(3));
+            Assert.AreEqual(27, 3.Raise(3));
+            Assert.AreEqual(16777216, 256.Raise(3));
+        }
+
+        [TestMethod]
+        public void LargePowersAreCorrect()
+        {
+            Assert.AreEqual(int.MaxValue, 2.Raise(31)-1);
+        }
+    }
+
+    [TestClass]
+    public class CountDigitTest
+    {
+        [TestMethod]
+        public void SingleDigitReturnsOne()
+        {
+            Assert.AreEqual(1, 1.CountDigits());
+        }
+
+        [TestMethod]
+        public void DoubleDigitsReturnsTwo()
+        {
+            Assert.AreEqual(2, 10.CountDigits());
+        }
+
+        [TestMethod]
+        public void NumberOfDigitsReturnCorrect()
+        {
+            Assert.AreEqual(3, 100.CountDigits());
+            Assert.AreEqual(4, 1000.CountDigits());
+            Assert.AreEqual(5, 10010.CountDigits());
+            Assert.AreEqual(10, 1001511000.CountDigits());
         }
     }
 }
--- ArmstrongNumber/C#/ArmstrongNumbers/ArmstrongNumbers/ArmstrongIntExtensions.cs
+++ ArmstrongNumber/C#/ArmstrongNumbers/ArmstrongNumbers/ArmstrongIntExtensions.cs
@@ -1,18 +1,53 @@
-﻿namespace ArmstrongNumber
+﻿using System;
+
+namespace ArmstrongNumber
 {
     public static class ArmstrongIntExtensions
     {
         public static bool IsArmstrong(this int number)
         {
-            int n = number, sum = 0;
-
-            while(number > sum)
+            int n = number, sum = 0, digits = number.CountDigits();
+            switch (digits)
             {
-                var r = n % 10;
-                sum += r * r * r;
+                case 1:
+                    return true;
+                case 2:
+                    return false;
+            }
+            while (n > 0)
+            {
+                var r = n%10;
+                sum += r.Power(digits);
+                if (sum > number)
+                    return false;
                 n /= 10;
             }
             return sum == number;
+        }
+
+        public static int CountDigits(this int number)
+        {
+            number = Math.Abs(number);
+            if (number < 10)
+                return 1;
+            return (number/10).CountDigits() + 1;
+        }
+
+        public static int Power(this int number, int power)
+        {
+            return number.Raise(power);
+        }
+        public static int Raise(this int number, int power)
+        {
+            if (number == 0 && power == 0) return 1;
+
+            if (power == 0) return 0;
+
+            if (power > 1) return number*number.Raise(power - 1);
+
+            if (power == 1) return number;
+
+            return 1;
         }
     }
 }
